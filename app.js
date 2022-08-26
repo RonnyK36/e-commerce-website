758f214ea671b2b4bfaeda8e35dc258aff1efec4
RonnyK36 <ronnykelvyne3@gmail.com>
2022-08-26 19:47:21 +0300

    Added responsive design to Nav bar

---
+++ app.js
@@ -0,0 +1,12 @@
+const menuBtn = document.getElementById("menu-icon")
+const menuItems = document.getElementById("menu-items")
+
+menuItems.style.maxHeight = "0px"
+
+menuBtn.addEventListener("click", () => {
+    if (menuItems.style.maxHeight == "0px") {
+        menuItems.style.maxHeight = "200px"
+    } else {
+        menuItems.style.maxHeight = "0px"
+    }
+})
