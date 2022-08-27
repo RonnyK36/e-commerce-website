75f2840e5d60eee8c278e325eba86a8751cd8ea7
RonnyK36 <ronnykelvyne3@gmail.com>
2022-08-27 18:21:57 +0300

    Added Products Detail screen

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const menuBtn = document.getElementById("menu-icon")
 const menuItems = document.getElementById("menu-items")
+const smallImg = document.getElementsByClassName("small-img")
+const productImg = document.getElementById("product-img")
+
 
 menuItems.style.maxHeight = "0px"
 
@@ -10,3 +13,14 @@ menuBtn.addEventListener("click", () => {
         menuItems.style.maxHeight = "0px"
     }
 })
+getImgs()
+    // JS for Product Details
+function getImgs() {
+    for (const img of smallImg) {
+        // console.log(img);
+        img.addEventListener('click', (e) => {
+            productImg.src = `images/gallery-${img.id}.jpg`
+
+        })
+    }
+}
