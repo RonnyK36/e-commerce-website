0f6d3d892381793a8466302510a10672d14f323e
RonnyK36 <ronnykelvyne3@gmail.com>
2022-08-28 18:20:47 +0300

    Added Login and Sign up Screen

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const menuBtn = document.getElementById("menu-icon")
 const menuItems = document.getElementById("menu-items")
 const smallImg = document.getElementsByClassName("small-img")
 const productImg = document.getElementById("product-img")
+const loginForm = document.getElementById("login-form")
+const registrationForm = document.getElementById("registration-form")
+const indicator = document.getElementById("indicator")
+
+const loginBtn = document.getElementById("login")
+const registerBtn = document.getElementById("register")
 
 
 menuItems.style.maxHeight = "0px"
@@ -24,3 +30,22 @@ function getImgs() {
         })
     }
 }
+
+// Toggle sign up and login form
+
+
+
+loginBtn.addEventListener("click", () => {
+    console.log("Login");
+
+    loginForm.style.transform = "translateX(0px)"
+    registrationForm.style.transform = "translateX(0px)"
+    indicator.style.transform = "translateX(0px)"
+})
+
+registerBtn.addEventListener("click", () => {
+    console.log("Create");
+    loginForm.style.transform = "translateX(300px)"
+    registrationForm.style.transform = "translateX(300px)"
+    indicator.style.transform = "translateX(100px)"
+})
